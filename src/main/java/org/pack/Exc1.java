d5671411c3045d38e2385c7a3b03602c2791ce82
Gayathri123-4 <gayurey0606@gmail.com>
2022-03-15 11:45:33 +0530

    gayucodes

---
+++ src/main/java/org/pack/Exc1.java
@@ -0,0 +1,35 @@
+package org.pack;
+
+public class Exc1 {
+	private void test1(){
+System.out.println("Test A2");
+	}
+private void test2() {
+System.out.println("Test A2");
+}
+public static void main(String[] args) {
+
+	Exc1 e=new Exc1();
+	e.test1();
+	e.test2();
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
